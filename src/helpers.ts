f4cc652b06bbd47e38871f00ca8ff37cdbc2485d
Alex Pearson <alex@alexpear.com>
2019-11-23 13:34:47 -0800

    :sparkles: Implement WebViews for derived concept not-graph

---
+++ src/helpers.ts
@@ -0,0 +1,13 @@
+import { MarkdownString } from 'vscode'
+
+export function buildBlurb(title = "", text = "", sourceUrls = []) {
+  const header = title && `## 💡 ${title}\n`;
+  const sources = `\n\nMore info: ${sourceUrls.map(url => `[${url}](${url})`).join('\n')}`;
+  const entry = `${header}${text}${sources}`;
+  const markdown = new MarkdownString(entry);
+
+  markdown.isTrusted = true;
+
+  return markdown;
+}
+
