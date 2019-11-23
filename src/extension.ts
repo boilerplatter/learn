aca1ff5a1e7aef45be6cd90504e04efe9df71aaa
Alex Pearson <alex@alexpear.com>
2019-11-22 23:40:21 -0800

    :point_up: Add super basic hover on tree-sat NodeTypes

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import * as vscode from "vscode";
 import * as Parser from "web-tree-sitter";
 import * as path from "path";
 import * as child_process from "child_process";
-import { workspace, WorkspaceFolder, window } from "vscode";
 
 import {
   LanguageClient,
@@ -14,6 +13,13 @@ import {
 } from "vscode-languageclient";
 
 const RUST_WASM_MODULE = "tree-sitter-rust";
+const RUST_HOVER_SCHEME = { language: "rust", scheme: "file" };
+const PROVIDED_LESSONS: Record<string, string> = {
+  try_expression: "This is some crazy-ass Result-unwrapping magic",
+  type_arguments: "These angle-y bois aren't HTML, we promise (see: JSX.rs)",
+  generic_type: "This type isn't basic... it's just generic",
+  unit_type: "This type is an absolute UNIT"
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -35,23 +41,44 @@ export async function activate(context: vscode.ExtensionContext) {
   // set Rust as standard tree-sitter language module
   parser.setLanguage(Rust);
 
+  // TODO: this should probably be constrained a bit
+  function getTree(document: any) {
+    // TODO: investigate getText vs Parser.input()
+
+    // parse tree of given document
+    return parser.parse(document.getText());
+  }
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let parseFile = vscode.commands.registerCommand("extension.parseFile", () => {
-    // The code you place here will be executed every time your command is executed
+  const parseFile = vscode.commands.registerCommand(
+    "extension.parseFile",
+    () => {
+      // register the hover action for Rust files
+      vscode.languages.registerHoverProvider(RUST_HOVER_SCHEME, {
+        provideHover(document, { line: row, character: column }, token) {
+          const tree = getTree(document);
+          
+          // TODO: check and refine this "parent" assumption
+          const { parent } = tree.rootNode.descendantForPosition({ row, column });
 
-    // get the parsed syntax trees
-    const trees = vscode.window.visibleTextEditors.map(editor =>
-      parser.parse(editor.document.getText())
-    ); // TODO: investigate getText vs Parser.input()
+          if(parent) {
+            let { nodeType } = parent.walk();
 
-    // do something with the parsed syntax trees
-    console.log("parse tree->", trees.map(tree => tree.rootNode.toString()));
+            return new vscode.Hover(PROVIDED_LESSONS[nodeType] || "")
+          }
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage("A file parse happened!");
-  });
+          return null;
+        }
+      });
+
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "A hover provider was registered for Rust files"
+      );
+    }
+  );
 
   spawnRustLSP();
   context.subscriptions.push(parseFile);
@@ -77,7 +104,9 @@ async function spawnRustLSP() {
     childProcess.on("error", (err: { code?: string; message: string }) => {
       if (err.code === "ENOENT") {
         console.error(`Could not spawn RLS: ${err.message}`);
-        window.showWarningMessage(`Could not spawn RLS: \`${err.message}\``);
+        vscode.window.showWarningMessage(
+          `Could not spawn RLS: \`${err.message}\``
+        );
       }
     });
     return childProcess;
