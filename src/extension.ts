d1052f25039b920c64b0dfc025ffc4d2b1e0001b
Alex Pearson <alex@alexpear.com>
2019-11-22 19:47:20 -0800

    :construction: Add initial tree-sitter parsing implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,45 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as Parser from 'web-tree-sitter';
+import * as path from 'path';
+
+
+const RUST_WASM_MODULE = 'tree-sitter-rust';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+        console.log('Initializing learn extension...');
+        
+        // block on parser initialization from WASM
+        await Parser.init(); // TODO: double-check that this doesn't crash, as the tree-sitter extension suggests 
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "learn-extension" is now active!');
+        // load Rust language module 
+        const absoluteRustPath = path.join(context.extensionPath, 'parsers', `${RUST_WASM_MODULE}.wasm`);
+        const Rust = await Parser.Language.load(absoluteRustPath); // TODO: cache these parsers between files or init on workspace open
+        const parser = new Parser();
+
+        // set Rust as standard tree-sitter language module
+        parser.setLanguage(Rust);
+
+        // get the parsed syntax trees
+        const trees = vscode.window.visibleTextEditors.map(editor => parser.parse(editor.document.getText())); // TODO: investigate getText vs Parser.input()
+
+        // do something with the parsed syntax trees
+        console.log(trees);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let parseFile = vscode.commands.registerCommand('extension.parseFile', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('A file parse happened!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(parseFile);
 }
 
 // this method is called when your extension is deactivated
